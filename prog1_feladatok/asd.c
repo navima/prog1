f07a82269d177cbf0ab08481727233d94934251e
navima <Draco.109@gmail.com>
2020-02-22 18:28:49 +0100

    Add project files.

---
+++ prog1_feladatok/asd.c
@@ -0,0 +1,21 @@
+#include <omp.h>
+#include <Windows.h>
+
+int inf()
+{
+	while (1);
+}
+
+int inf2()
+{
+	while (1)
+		sleep(1);
+}
+
+int inf3()
+{
+#pragma omp parralel
+	{
+		while (1);
+	}
+}
---
+++ prog1_feladatok/prog1_feladatok.cpp
@@ -0,0 +1,6 @@
+#include "asd.c"
+
+int main()
+{
+	inf();
+}
