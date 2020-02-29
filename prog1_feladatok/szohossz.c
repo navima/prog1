ef9f17574733171f79de1fdbd20775a1efffe423
navima <draco.109@gmail.com>
2020-02-29 23:00:12 +0100

    Amended szohossz with bitshifting solution

--- prog1_feladatok/szohossz.c
+++ prog1_feladatok/szohossz.c
@@ -3,13 +3,24 @@ int szohossz()
 	int good_timer_count = 2;
 	int hossz = 0;
 
+	//Beépített keyworddel
 	while (good_timer_count > 1)
 	{
 		hossz = sizeof(int) * 8;
 		good_timer_count = 0;
 	}
 
-	int asd = 1;
+	//Bitshifteléssel
+	int x = 1;
+	int counter = 0;
+	while (x!=0)
+	{
+		x = x << 1;
+		counter++;
+	};
+
+	printf(hossz);
+	printf(counter);
 
 	return 0;
 }
