6f9b1420c5fd9c5c7f91b7e26c58246515310c59
navima <draco.109@gmail.com>
2020-03-01 22:32:36 +0100

    LZW

---
+++ prog1_feladatok/lzw.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct fa fa;
+
+struct fa
+{
+	fa* left;
+	fa* right;
+	int value;
+};
+
+int fa_insert(fa* target, char* toInsert,int length)
+{
+	fa* curr = target;
+	int strIndex = 0;
+	int success = 0;
+
+	while (!success && strIndex < length)
+	{
+		if (toInsert[strIndex] = '0')
+			if (curr->left)
+				curr = curr->left, strIndex++;
+			else
+				curr->left = (fa*) malloc(sizeof(fa)), curr->left->value = 0,curr->left->left = 0, curr->left->right = 0, strIndex++, success = 1;
+		else
+			if (curr->right)
+				curr = curr->right, strIndex++;
+			else
+				curr->right = (fa*)malloc(sizeof(fa)), curr->left->value = 1, curr->left->left = 0, curr->left->right = 0, strIndex++, success = 1;
+	}
+	return 0;
+}
+
+int lzw()
+{
+	fa myFa = {0, 0, -1};
+
+	char asd[4] = { '0','1','0','0' };
+
+	fa_insert(&myFa, asd, 3);
+
+	return 0;
+}
