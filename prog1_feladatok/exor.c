171a9b25f0ecd6074c5b830dcf04104f6380ae5d
navima <draco.109@gmail.com>
2020-03-03 21:14:38 +0100

    Implemented xor_crack_variable to crack length<8 key ciphers

--- prog1_feladatok/exor.c
+++ prog1_feladatok/exor.c
@@ -3,6 +3,7 @@
 #include "stdio.h"
 #include "string.h"
 #include "ctype.h"
+#include "stdlib.h"
 
 //https://stackoverflow.com/a/23618467/9281022
 char* strlwr_(char *str)
@@ -35,8 +36,8 @@ int xor_encrypt(char* source, size_t size, char* destination, char* key, size_t 
 
 int xor_decrypt(char* source, size_t size, char* destination, char* key, size_t sizeKey) { return xor_encrypt(source, size, destination, key, sizeKey); }
 
-unsigned int count_ciphertext[128];
-float distribution_ciphertext[128];
+unsigned int count_ciphertext[256];
+float distribution_ciphertext[256];
 int xor_crack_1byte(char* source, size_t size, char* destination)
 {
 	//Count occurences of characters in the ciphertext
@@ -45,7 +46,8 @@ int xor_crack_1byte(char* source, size_t size, char* destination)
 		count_ciphertext[(unsigned char)source[i]]++;
 
 
-	i = 128;
+	//Count distribution
+	i = 256;
 	while (i-->0)
 	{
 		if (count_ciphertext[i] > 0)
@@ -57,16 +59,15 @@ int xor_crack_1byte(char* source, size_t size, char* destination)
 
 	//Associating max frequency with space
 	int max_cipher_index = 0;
-	int max_english_index = 4;
-	unsigned char key = 0;
 
-	for (int k = 0; k < 128; k++)
+	for (int k = 0; k < 256; k++)
 	{
 		if (distribution_ciphertext[k] > distribution_ciphertext[max_cipher_index])
 			max_cipher_index = k;
 	}
 
-	key = 32^max_cipher_index;
+	unsigned char key = (unsigned char)' ' ^ (unsigned char)max_cipher_index;
+
 
 	i = size;
 	while (i-- > 0)
@@ -76,10 +77,50 @@ int xor_crack_1byte(char* source, size_t size, char* destination)
 	return 0;
 }
 
+int xor_crack_variable(char* source, size_t size, char* destination, int keyLength)
+{
+	char *asd[8];
+
+	for (int i = 0; i < keyLength; i++)
+	{
+		asd[i] = (char*)malloc(sizeof(char)*(size / keyLength) + 1 + 1);
+	}
+
+	for (int i = 0; i < size; i += keyLength)
+	{
+		for (int j = 0; j < keyLength; j++)
+		{
+			asd[j][i / keyLength] = source[i + j];
+		}
+		
+	}
+
+	for (int i = 0; i < keyLength; i++)
+	{
+		//printf(asd[i]);
+		xor_crack_1byte(asd[i], (size / keyLength) + 2, asd[i]);
+	}
+
+
+
+	for (int i = 0; i < size; i += keyLength)
+	{
+		for (int j = 0; j < keyLength; j++)
+		{
+			destination[i+j] = asd[j][i / keyLength];
+		}
+
+	}
+
+
+
+	return 0;
+}
+
 int xor_test()
 {
 
-	char plaintext[] = "Why would you want to program? Our civilization runs on software. Withoutunderstanding software you are reduced to believing in “magic” and will belocked out of many of the most interesting, profitable, and socially useful technicalfields of work. When I talk about programming, I think of the whole spectrum ofcomputer programs from personal computer applications with GUIs (graphicaluser interfaces), through engineering calculations and embedded systems controlapplications (such as digital cameras, cars, and cell phones), to text manipulationapplications as found in many humanities and business applications. Like mathematics,programming — when";
+	char plaintext[] = "Why would you want to program? Our civilization runs on software. Without understanding software you are reduced to believing in “magic” and will be locked out of many of the most interesting, profitable, and socially useful technical fields of work. When I talk about programming, I think of the whole spectrum of computer programs from personal computer applications with GUIs (graphical user interfaces), through engineering calculations and embedded systems control applications (such as digital cameras, cars, and cell phones), to text manipulation applications as found in many humanities and business applications.";
 	size_t size = strlen(plaintext);
 	strlwr_(plaintext);
 
@@ -93,14 +134,16 @@ int xor_test()
 	decodetext[680] = 0;
 	cracktext[680] = 0;
 
-	char key[] = "á";
+	char key[] = {1,2};
 	size_t sizeKey = strlen(key);
 
 	xor_encrypt(plaintext, size, ciphertext, key, sizeKey);
 
 	xor_decrypt(ciphertext, size, decodetext, key, sizeKey);
  
-	xor_crack_1byte(ciphertext, size, cracktext);
+	//xor_crack_1byte(ciphertext, size, cracktext);
+
+	xor_crack_variable(ciphertext, size, cracktext, 2);
 
 	printf("%s\n\n", plaintext);
 	printf("%s\n\n", ciphertext);
