2e9dd51c28fb3f73ecc147752a6e45f5bcb4a4b6
navima <draco.109@gmail.com>
2020-03-20 13:19:04 +0100

    Cleaned up cuda stuff

--- prog1_feladatok_CUDA/kernel.cu
+++ prog1_feladatok_CUDA/kernel.cu
@@ -5,11 +5,10 @@
 #include <iomanip>
 #include <fstream>
 #include <algorithm>
-#include <string>
 
 __global__ void mappingSquareKernel(unsigned short int* dest, const float* cReal, const float* cImag)
 {
-	int i = threadIdx.x+(blockIdx.x*blockDim.x);
+	int i = threadIdx.x + (blockIdx.x*blockDim.x);
 
 	float zRealLoc = cReal[i];
 	float zImagLoc = cImag[i];
@@ -21,16 +20,12 @@ __global__ void mappingSquareKernel(unsigned short int* dest, const float* cReal
 		float temp = zRealLoc * zRealLoc - zImagLoc * zImagLoc + cReal[i];
 		zImagLoc = 2.0 * zRealLoc * zImagLoc + cImag[i];
 		zRealLoc = temp;
-		//k++;
 
 		if (fabs(zRealLoc) > 2.0 || fabs(zImagLoc) > 2.0)
-		{
 			break;
-		}
 	}
 
 	dest[i] = k;
-	//dest[i] = i;
 }
 
 __global__ void mappingBiomKernel(unsigned short int* dest, const float* cReal, const float* cImag)
@@ -41,7 +36,7 @@ __global__ void mappingBiomKernel(unsigned short int* dest, const float* cReal, 
 	float zImagLoc = cImag[i];
 
 	float dR = -0.8;
-	float dI= 0.156;
+	float dI = 0.156;
 
 	float vR = 0;
 	float vI = 0;
@@ -59,81 +54,61 @@ __global__ void mappingBiomKernel(unsigned short int* dest, const float* cReal, 
 		zImagLoc = 2.0 * vR * vI + dI;
 
 		if (fabs(zRealLoc) > 2.0 || fabs(zImagLoc) > 2.0)
-		{
 			break;
-		}
 	}
 
 	dest[i] = k;
-	//dest[i] = i;
 }
 
-
-void mandelWithCuda(unsigned short int* iter, const float* bR,const float* bI, unsigned int WIDTH, unsigned int HEIGHT)
+void mandelWithCuda(unsigned short int* iter, const float* bR, const float* bI, unsigned int WIDTH, unsigned int HEIGHT)
 {
 	unsigned int size = WIDTH * HEIGHT;
 
 	float* dev_bR = 0;
 	float* dev_bI = 0;
 	unsigned short int* dev_iter = 0;
-	
 
 	cudaError asd;
 
-	// Choose which GPU to run on, change this on a multi-GPU system.
 	cudaSetDevice(0);
 
-	// Allocate GPU buffers for three vectors (two input, one output)
-	asd =cudaMalloc((void**)&dev_iter, size * sizeof(unsigned short));
-	asd =cudaMalloc((void**)&dev_bR, size * sizeof(float));
-											  
-	asd=cudaMalloc((void**)&dev_bI, size * sizeof(float));
+	asd = cudaMalloc((void**)&dev_iter, size * sizeof(unsigned short));
+	asd = cudaMalloc((void**)&dev_bR, size * sizeof(float));
+	asd = cudaMalloc((void**)&dev_bI, size * sizeof(float));
 
 
-	// Copy input vectors from host memory to GPU buffers.
-	asd=cudaMemcpy(dev_bR, bR, size * sizeof(float), cudaMemcpyHostToDevice);
-										 
-	asd=cudaMemcpy(dev_bI, bI, size * sizeof(float), cudaMemcpyHostToDevice);
+	asd = cudaMemcpy(dev_bR, bR, size * sizeof(float), cudaMemcpyHostToDevice);
+	asd = cudaMemcpy(dev_bI, bI, size * sizeof(float), cudaMemcpyHostToDevice);
 
-
-	// Launch a kernel on the GPU with one thread for each element.
 
 	dim3 dimBlock = dim3(std::min(1024u, size));;
 	dim3 dimGrid = dim3(std::ceil((float)size / 1024));
 
-	mappingSquareKernel <<<dimGrid, dimBlock>>> (dev_iter, dev_bR, dev_bI);
+	mappingSquareKernel << <dimGrid, dimBlock >> > (dev_iter, dev_bR, dev_bI);
 
-	// Check for any errors launching the kernel
-	
 	asd = cudaGetLastError();
 
-	// cudaDeviceSynchronize waits for the kernel to finish, and returns
-	// any errors encountered during the launch.
+
 	cudaDeviceSynchronize();
 
-	// Copy output vector from GPU buffer to host memory.
 	cudaMemcpy(iter, dev_iter, size * sizeof(unsigned short), cudaMemcpyDeviceToHost);
 }
 
 int main()
 {
-    const unsigned int WIDTH = 4096;
-	const unsigned int HEIGHT = 4096;
+	const unsigned int WIDTH = 512;
+	const unsigned int HEIGHT = 512;
 
-    float* bReal = new float[WIDTH*HEIGHT];
+	float* bReal = new float[WIDTH*HEIGHT];
 	float* bImag = new float[WIDTH*HEIGHT];
 	unsigned short int* iter = new unsigned short int[WIDTH*HEIGHT];
 
 	for (int i = 0; i < WIDTH; i++)
-	{
 		for (int j = 0; j < HEIGHT; j++)
 		{
 			bReal[i*HEIGHT + j] = (float)i / WIDTH * 4.0 - 2.0;
 			bImag[i*HEIGHT + j] = (float)j / HEIGHT * 4.0 - 2.0;
-			//std::cout << std::setw(6) << std::left << std::fixed <<std::setprecision(2)<<bImag[i*HEIGHT + j];
 		}
-		//std::cout << "\n";
-	}
 
 
 	mandelWithCuda(iter, bReal, bImag, WIDTH, HEIGHT);
@@ -149,16 +124,8 @@ int main()
 	out << "63\n";
 
 	for (int i = 0; i < WIDTH; i++)
-	{
-		for (int j = 0; j < HEIGHT; j++)
-		{
-			//std::cout << std::setw(4) << std::left << iter[i*HEIGHT + j];
-			out << iter[i*HEIGHT + j] << " ";//(unsigned char)iter[i*WIDTH + j];
-			//out <<std::setw(4)<<std::left<< iter[i*HEIGHT+j] <<" ";
-		}
-		//std::cout << "\n";
-		out << "\n";
-	}
+		for (int j = 0; j < HEIGHT; j++, out << "\n")
+			out << iter[i*HEIGHT + j] << " ";
 
 	out.close();
 
