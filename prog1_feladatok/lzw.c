fc5fa16e08b1c1ddfe5fcc36766b365161cfe9a6
navima <draco.109@gmail.com>
2020-04-23 15:22:01 +0200

    stuff

--- prog1_feladatok/lzw.c
+++ prog1_feladatok/lzw.c
@@ -1,48 +1,55 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//Hogy tudja saját magát tartalmazni a fa, itt deklarájuk, hogy létezik
 typedef struct fa fa;
 
+//A fa struktúránk
 struct fa
 {
-	fa* left;
-	fa* right;
+	fa *left;
+	fa *right;
 	int value;
 };
 
-int fa_insert(fa* target, char* toInsert,int length)
+//Standard LZW beillesztés
+int fa_insert(fa *target, char *toInsert, int length)
 {
-	fa* curr = target;
-	int strIndex = 0;
-	int success = 0;
+	fa *curr = target;
+	int strIndex = 0; //Hol tartunk a beillesztendő sztingben
+	int success = 0;  //sikerült-e beilleszteni
 
-	while (!success && strIndex < length)
+	while (strIndex < length)	//addig megyünk amí van a stringünkből
 	{
-		if (toInsert[strIndex] = '0')
-			if (curr->left)
-				curr = curr->left, strIndex++;
-			else
-				curr->left = (fa*) malloc(sizeof(fa)), curr->left->value = 0,curr->left->left = 0, curr->left->right = 0, strIndex++, success = 1;
-		else
-			if (curr->right)
+		success = 0;
+		curr = target;
+		while (!success && strIndex < length) //addig megyünk amíg nem lett sikeres egy beillesztés vagy a stringünk végére nem értünk
+		{
+			if (toInsert[strIndex] = '0') //balra szúrunk be
+				if (curr->left)
+					curr = curr->left, strIndex++; //már volt ott érték, tehát folytatjuk a beszúrást azon a részfán
+				else
+					curr->left = (fa *)malloc(sizeof(fa)), curr->left->value = 0, curr->left->left = 0, curr->left->right = 0, strIndex++, success = 1; //új részfát illesztünk be, majd jelezzük, hogy sikerült a beszúrás.
+			else if (curr->right)	//ugyanez jobbra
 				curr = curr->right, strIndex++;
 			else
-				curr->right = (fa*)malloc(sizeof(fa)), curr->left->value = 1, curr->left->left = 0, curr->left->right = 0, strIndex++, success = 1;
+				curr->right = (fa *)malloc(sizeof(fa)), curr->left->value = 1, curr->left->left = 0, curr->left->right = 0, strIndex++, success = 1;
+		}
 	}
 	return 0;
 }
 
-int preorder(fa* target)
+int preorder(fa *target)
 {
 	//do stuff
 	if (target->left)
 		preorder(target->left);
-	if(target->right)
+	if (target->right)
 		preorder(target->right);
 	return 0;
 }
 
-int postorder(fa* target)
+int postorder(fa *target)
 {
 	if (target->left)
 		postorder(target->left);
@@ -56,9 +63,9 @@ int lzw()
 {
 	fa myFa = {0, 0, -1};
 
-	char asd[6] = { '0','1','0','0', '0', '1' };
+	char asd[7] = {'0', '1', '0', '0', '0', '1', 0};
 
-	fa_insert(&myFa, asd, 3);
+	fa_insert(&myFa, asd, 5);
 
 	return 0;
 }
